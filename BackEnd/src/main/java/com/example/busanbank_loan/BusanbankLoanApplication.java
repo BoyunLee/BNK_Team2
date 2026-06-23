81c30b98281a9ef806db6f9e030abb99ac417aab
kokeunho <kokangsik65@naver.com>
2026-06-23 19:52:56 +0900

    init: BE project init

---
+++ BackEnd/src/main/java/com/example/busanbank_loan/BusanbankLoanApplication.java
@@ -0,0 +1,13 @@
+package com.example.busanbank_loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusanbankLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusanbankLoanApplication.class, args);
+	}
+
+}
